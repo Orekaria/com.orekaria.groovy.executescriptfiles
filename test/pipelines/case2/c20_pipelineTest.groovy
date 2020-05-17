c3861cdc4631bdbbf5dba909e881a1c9e5a63dca
Rafa Alonso <ral@openbravo.com>
2020-05-17 14:04:40 +0200

    Add a test to verify that we are thread safe (!) with the script.getBinding approach
    - other methods, like .metaclass. or Expando cannot achieve thread safety, at least with this degree of simplyness

--- test/pipelines/case2/c20_pipelineTest.groovy
+++ test/pipelines/case2/c20_pipelineTest.groovy
@@ -2,24 +2,19 @@ package pipelines.case2
 
 import src.case2.Pipeline
 
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+
 class c20_pipelineTest extends GroovyTestCase {
 
-   void test() {
+   void test_minimal() {
       def pipeline = new Pipeline(this)
       pipeline()
       assert pipeline.config != null
    }
 
-   void test2() {
-      def pipeline = new Pipeline(this)
-      pipeline([
-         some_key: 'some_value'
-      ])
-      assert pipeline.config != null
-      assert pipeline.config.some_key == 'some_value'
-   }
-
-   void test3() {
+   void test_Map() {
       def pipeline = new Pipeline(this)
       pipeline([
          some_key: 'some_value'
@@ -40,6 +35,66 @@ class c20_pipelineTest extends GroovyTestCase {
       })
       pipeline()
       assert count == 3
+   }
+
+   /**
+    * This is a very important test
+    * There are several ways to inject and intercept methods in Groovy objects
+    * For example, using .metaClass.
+    * But testing requires that the objects are completely isolated
+    */
+   void test_threadSafe() {
+      def pipeline = new Pipeline(this)
+      def pipeline2 = new Pipeline(this)
+      def count = 0
+      def count2 = 0
+      pipeline.mock('echo', { Object s -> // methods can be intercepted
+         println('intercepted!')
+         count += 1
+         sleep(10)
+      })
+      pipeline2.mock('echo', { Object s -> // methods can be intercepted
+         println('intercepted!')
+         count2 += 1
+      })
+
+      def threadPool = Executors.newFixedThreadPool(4)
+      List<Future> futures = new ArrayList<>()
+      futures.add(threadPool.submit({
+         pipeline([
+            some_key: 'some_value'
+         ])
+      } as Callable<Integer>))
+      futures.add(threadPool.submit({
+         pipeline2([
+            some_key2: 'some_value2'
+         ])
+      } as Callable<Integer>))
+      futures.each { it.get() }
+
+      assert count == 3
+      assert count2 == 3
+
+      assert pipeline.config.some_key == 'some_value'
+      assert pipeline2.config.some_key2 == 'some_value2'
+      assert pipeline.config.respondsTo('some_key2') == []
+      assert pipeline2.config.respondsTo('some_key') == []
+   }
+
+   /**
+    * Properties cannot be intercepted with .mock
+    */
+   void test_interceptProperty() {
+      def pipeline = new Pipeline(this)
+      def count = 0
+      pipeline.mock('config', { -> // properties can be intercepted
+         println('intercepted!')
+         count += 1
+      })
+      pipeline()
+      shouldFail {
+         assert count == 3
+      }
    }
 
 }
